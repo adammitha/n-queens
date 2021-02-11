c4b21b9af42728cc4955d5e5d9ae55d4b419fcd4
Adam Mitha <adam.mitha@gmail.com>
2021-02-10 20:10:03 -0800

    Added error handling for non-integer arguments

--- main.go
+++ main.go
@@ -11,7 +11,10 @@ func main() {
 	if len(os.Args[1:]) != 1 {
 		log.Fatal("Please supply the size of the chess board as a single integer")
 	}
-	n, _ := strconv.Atoi(os.Args[1])
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal("Please supply the size of the chess board as a single integer")
+	}
 
 	emptyBoard := NewBoard(n)
 	sol, err := Solve(emptyBoard)
