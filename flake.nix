236733442919ea2af1a1387e2cafd6bb5c980a8a
Adam Mitha <adam.mitha@gmail.com>
2023-09-12 22:59:35 -0700

    Update flake description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A Nix-flake-based Go 1.17 development environment";
+  description = "A Nix-flake-based Go 1.21 development environment";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
